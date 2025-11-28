7d67fcd2630fa5167f8c5524299556b07dcc9489
Pritika Bugga <144397907+pritikabugga@users.noreply.github.com>
2025-11-28 01:34:15 -0500

    refactor: guide.py to insert access token placeholder

--- guide.py
+++ guide.py
@@ -1,6 +1,6 @@
 from wit import Wit 
 
-access_token = "EYSSLBM6XTFMLFON7PIJNFNDA6QWORGZ"
+access_token = "" #Insert token
 client = Wit(access_token = access_token) #Input access token for Wit API
 
 message_text = input("Ask a message: " ) #Type in question for Fall 2024 courses at UNCC
@@ -12,3 +12,4 @@ value = resp['entities'][entity][0]['value']
 print(value) #Intended Response is given
 
 #This project is licensed under the MIT License.
+
